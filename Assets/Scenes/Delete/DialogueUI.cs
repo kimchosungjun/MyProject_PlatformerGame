dd6ad79fd7b08e262cd7686bff275a1d5f3ea637
sung jun <chosung0531@naver.com>
2024-07-03 19:26:44 +0900

    0703

--- Assets/Scenes/Delete/DialogueUI.cs
+++ Assets/Scenes/Delete/DialogueUI.cs
@@ -6,57 +6,86 @@ using TMPro;
 
 public class DialogueUI : MonoBehaviour
 {
+    [Header("기획자 Part")]
+    [SerializeField, Tooltip("다음 대화를 자동으로 넘기는지 결정하는 것")] bool isAutoDialouge;
+    [SerializeField, Range(0.1f, 0.5f), Tooltip("타이핑 속도")] float typeTime;
+    [SerializeField, Range(0.1f, 1f), Tooltip("대화가 자동으로 넘어가는 속도")] float nextDialogueTime;
+
+    [Header("개발자 Part")]
+    [SerializeField] GameObject onoffUI;
+    [SerializeField] GameObject skipTextObject;
     [SerializeField] TextMeshProUGUI timeText;
     [SerializeField] TextMeshProUGUI dialogueText;
 
-    [SerializeField] int _ID;
-    [SerializeField, Range(0.1f, 0.5f)] float typeTime;
-    [SerializeField, Range(0.1f,1f)] float nextDialogueTime;
-    Conversation conversation = new Conversation();
-
-    [SerializeField] int curIdx = 0;
-    bool isStart = false;
+    int storyID =1;
+    public int StoryID { get { return storyID; } set { storyID = value; } }
+    Dialogue dialogue = new Dialogue();
+    int curIdx = 0; // Current Dialogue Index
+    bool isStartDialogue = false; // Prevent Overlap Dialogue
+    bool isDoneDialogue = false; // Show Next Dialouge Key
+    
     public void LoadDiagloue()
     {
         curIdx = 0;
-        conversation = ConversationManager.Instance.LoadDialogue(_ID);
+        dialogue = DialogueManager.Instance.LoadDialogue(storyID);
+        if (dialogue == null)
+            return;
+        timeText.text = dialogue.storyTime;
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.C) && !isStart)
+        if (Input.GetKeyDown(KeyCode.C) && !isStartDialogue)
             StartDialogue();
+        if(Input.anyKeyDown && isDoneDialogue)
+        {
+            CheckNextDialogue();
+        }
     }
 
     public void StartDialogue()
     {
-        isStart = true;
+        if (dialogue == null)
+            return;
+        onoffUI.SetActive(true);
+        isStartDialogue = true;
         LoadDiagloue();
         StartCoroutine(DialogueCor());
     }
 
     public IEnumerator DialogueCor()
     {
-        int len = conversation.lines[curIdx].Length;
+        isDoneDialogue = false;
+        int len = dialogue.storyLines[curIdx].Length;
         dialogueText.text = "";
         for (int i = 0; i < len; i++)
         {
-            dialogueText.text += conversation.lines[curIdx][i];
+            dialogueText.text += dialogue.storyLines[curIdx][i];
             yield return new WaitForSeconds(typeTime);
         }
         curIdx += 1;
-        yield return new WaitForSeconds(nextDialogueTime);
-        CheckNextDialogue();
+        if (isAutoDialouge)
+        {
+            yield return new WaitForSeconds(nextDialogueTime);
+            CheckNextDialogue();
+        }
+        else
+        {
+            isDoneDialogue = true;
+            skipTextObject.SetActive(true);
+        }
     }
 
     public void CheckNextDialogue()
     {
-        int len = conversation.lines.Count;
+        int len = dialogue.storyLines.Count;
+        skipTextObject.SetActive(false);
         if (curIdx >= len)
         {
             // 대화 종료
-            isStart = false;
-            gameObject.SetActive(false);
+            isStartDialogue = false;
+            isDoneDialogue = false;
+            onoffUI.SetActive(false);
         }
         else
         {
